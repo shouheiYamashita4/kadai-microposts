814dc5063d0418270b0152ad287f23eb0bf6db9e
shouhei.yamashita4 <shouhei.yamashita4@gmail.com>
2020-01-19 13:33:32 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to Microposts</h1>
+            {!! link_to_route('signup.get', 'Sing Up Now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
